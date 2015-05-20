a30bd0a05bbf5f41f095134f9026b1100ea1c194
Spacks <Spacks@users.noreply.github.com>
2015-05-19 22:28:24 -0400

    Removed fieldName param and field

--- src/pw/slacks/stringdeobfuscator/ObfuscatedString.java
+++ src/pw/slacks/stringdeobfuscator/ObfuscatedString.java
@@ -9,17 +9,12 @@ import lombok.ToString;
 public class ObfuscatedString {
 
     @Getter private final int progessId, nextId, id;
-    @Getter private final String obfuscatedString, fieldName;
+    @Getter private final String obfuscatedString;
 
     public ObfuscatedString(int id, int nextId, int progessId, String obfuscatedString){
-        this(id, nextId, progessId, obfuscatedString, "");
-    }
-
-    public ObfuscatedString(int id, int nextId, int progessId, String obfuscatedString, String fieldName){
         this.progessId = progessId;
         this.nextId = nextId;
         this.id = id;
         this.obfuscatedString = obfuscatedString;
-        this.fieldName = fieldName;
     }
 }
